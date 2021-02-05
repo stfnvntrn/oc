f465fba0e99e7c473b080ae1beece6140bc23584
stfnvntrn <78547904+stfnvntrn@users.noreply.github.com>
2021-02-05 17:48:26 +0100

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift!  from SV"
+		response = "Buon week end from SV"
 	}
 
 	fmt.Fprintln(w, response)
